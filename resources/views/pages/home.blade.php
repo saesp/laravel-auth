50fa3f6cb1591aa27789b6bc2833c8d78a75bbd4
esposito.00@outlook.it <esposito.00@outlook.it>
2023-02-09 18:17:27 +0100

    added ucfirst()

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,13 +6,14 @@
 <ul>
     @foreach ($projects as $project)
     <li class="mb-3">
-        <h3><a href="{{route('project.show', $project) }}">{{$project->name}}</a></h3>
+        <h3><a href="{{route('project.show', $project) }}">{{ucfirst($project->name)}}</a></h3>
+        <div>{{$project->release_date}}</div>
         <ul>
             <li>
-                <i><strong>languages:</strong></i>  {{$project->languages}}
+                <i><strong>Languages:</strong></i>  {{$project->languages}}
             </li>
             <li>
-                <i><strong>completed:</strong></i> {{$project->completed == 0 ? 'No' : 'Yes'}}
+                <i><strong>Completed:</strong></i> {{$project->completed == 0 ? 'No' : 'Yes'}}
             </li>
         </ul>
     </li>
